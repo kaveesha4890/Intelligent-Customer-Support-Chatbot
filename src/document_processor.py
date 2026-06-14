ccf2745af50b9a891af2b54662937af0efdfa460
kaveesha wijegurusinghe <155706266+kaveesha4890@users.noreply.github.com>
2026-06-14 13:34:12 +0530

    updated file

--- src/document_processor.py
+++ src/document_processor.py
@@ -1,6 +1,6 @@
 # src/document_processor.py
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def load_and_chunk_documents(kb_path="knowledge_base"):
     loader = DirectoryLoader(kb_path, glob="**/*.txt", loader_cls=TextLoader)
